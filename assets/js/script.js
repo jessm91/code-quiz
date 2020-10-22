8d651cee8a5e6aa8b5f9bc0a5c3749b7f3c5f05f
Jessica Mangiameli <jmangiam@asu.edu>
2020-10-22 16:42:51 -0500

    updated questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,180 +1,159 @@
-var questions = [
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let timer = 75;
+let availableQuestions = [];
+
+let questions = [
     {
         question: 'What is the first thing you should do before writing a single line of HTML code?',
-        answers: [
-            { text: 'Sketch out what the webpage should look like', correct: true },
-            { text: 'Write all of your CSS code', correct: false },
-            { text: 'Download the latest version of VS Code', correct: false },
-            { text: 'Publish your webpage on GitHub', correct: false }
-        ]
+             choice1: 'Sketch out what the webpage should look like',
+             choice2: 'Write all of your CSS code',
+             choice3: 'Download the latest version of VS Code',
+             choice4: 'Publish your webpage on GitHub',
+             answer: 1
     },
     {
         question: 'On a technical level, what is the difference between a <section> element and a <div>?',
-        answers: [
-            { text: 'There is no difference', correct: true },
-            { text: 'A <div> element takes up more width on the page', correct: false },
-            { text: 'A <section> element has bold text by default', correct: false },
-            { text: 'A <section> element takes up more height on the page', correct: false }
-        ]
+             choice1: 'There is no difference',
+             choice2: 'A <div> element takes up more width on the page',
+             choice3: 'A <section> element has bold text by default',
+             choice4: 'A <section> element takes up more height on the page',
+             answer: 1
     },
     {
         question: 'How do you leave hidden comments in HTML code?',
-        answers: [
-            { text: '/* */', correct: false },
-            { text: '<!-- -->', correct: true },
-            { text: '{{ }}', correct: false },
-            { text: '//', correct: false }
-        ]
+            choice1: '/* */',
+            choice2: '<!-- -->',
+            choice3: '{{ }}',
+            choice4: '//',
+            answer: 2
     },
     {
         question: 'How do you add a message to a git commit?',
-        answers: [
-            { text: 'Upload and attach a file to git commit', correct: false },
-            { text: 'Add -m "message" at the end of git commit', correct: true },
-            { text: 'Both A & B', correct: false },
-            { text: 'None of the above', correct: false }
-        ]
+            choice1: 'Upload and attach a file to git commit',
+            choice2: 'Add -m "message" at the end of git commit',
+            choice3: 'Both A & B',
+            choice4: 'None of the above',
+            answer: 2
     },
     {
-        question: 'Which of the following is NOT a good reason for version control?',
-        answers: [
-            { text: 'Version control allows the codebase to be modified and tested without interrupting the user experience', correct: false },
-            { text: 'Version control allows changes to the codebase to be tested individually', correct: false },
-            { text: 'Version control allows teams to work on individual features synchronously', correct: false},
-            { text: 'Version control allows features to ship directly to the main branch', correct: true }
-        ]
+        question: 'Which of the following is NOT a good reason for version control?', 
+             choice1: 'Version control allows the codebase to be modified and tested without interrupting the user experience',
+             choice2: 'Version control allows changes to the codebase to be tested individually',
+             choice3: 'Version control allows teams to work on individual features synchronously',
+             choice4: 'Version control allows features to ship directly to the main branch',
+             answer: 4
     },
     {
         question: 'What is the correct syntax to close the <title> element?',
-        answers: [
-            { text: '</title>', correct: true },
-            { text: '<!title>', correct: false },
-            { text: '<?title>', correct: false },
-            { text: '<endtitle>', correct: false }
-        ]
+            choice1: '</title>',
+            choice2: '<!title>',
+            choice3: '<?title>',
+            choice4: '<endtitle>',
+            answer: 1
     },
     {
         question: 'What does CSS stand for?',
-        answers: [
-            { text: 'Cascading Style Sheets', correct: true },
-            { text: 'Colorful Style Sheets', correct: false },
-            { text: 'Computer Style Sheets', correct: false },
-            { text: 'Creative Style Sheets', correct: false}
-        ]
+            choice1: 'Cascading Style Sheets',
+            choice2: 'Colorful Style Sheets',
+            choice3: 'Computer Style Sheets',
+            choice4: 'Creative Style Sheets',
+            answer: 1
     },
     {   
         question: 'What is the purpose of the alt attribute for images?',
-        answers: [
-            { text: 'To make the image load faster', correct: false },
-            { text: 'To make it easier to style the image with CSS', correct: false},
-            { text: 'To prevent search engines from indexing the image', correct: true},
-            { text: 'To provide context for the image', correct: true }
-        ]
+            choice1: 'To make the image load faster',
+            choice2: 'To make it easier to style the image with CSS',
+            choice3: 'To prevent search engines from indexing the image',
+            choice4: 'To provide context for the image',
+            answer: 4
     },
     {
         question: 'How can you add more than one class to an HTML element?',
-        answers: [
-            { text: 'Add a second class attribute', correct: false },
-            { text: 'Add a comma between the class names', correct: false },
-            { text: 'Add a space between the class names', correct: true },
-            { text: 'Add a class-2 attribute', correct: false }
-        ]
+            choice1: 'Add a second class attribute',
+            choice2: 'Add a comma between the class names',
+            choice3: 'Add a space between the class names',
+            choice4: 'Add a class-2 attribute',
+            answer: 3
     },
     {
         question: 'What CSS delcaration could you add to a 50%-width <div> to center it?',
-        answers: [
-            { text: 'text-align: center', correct: false },
-            { text: 'margin: 0 auto', correct: true },
-            { text: 'float: center', correct: false },
-            { text: 'align: center', correct: false }
-        ]
+            choice1: 'text-align: center',
+            choice2: 'margin: 0 auto',
+            choice3: 'float: center',
+            choice4: 'align: center',
+            answer: 2
     },
     {
         question: 'Which CSS property allows the parent element to display its CSS properties by stretching its dimensions to physically contain its child elements?',
-        answers: [
-            { text: 'text-align: center;', correct: false },
-            { text: 'overflow: auto;', correct: true },
-            { text: 'margin: auto;', correct: false },
-            { text: 'display: inline-block;', correct: false }
-        ]
+            choice1: 'text-align: center;',
+            choice2: 'overflow: auto;',
+            choice3: 'margin: auto;',
+            choice4: 'display: inline-block;',
+            answer: 2
     },
     {
         question: 'If I wish to align an element to the top of its container, which CSS property should I use?',
-        answers: [
-            { text: 'text-align: top;', correct: false },
-            { text: 'margin-align: top;', correct: false },
-            { text: 'overflow: top;', correct: false },
-            { text: 'vertical-align: top;', correct: true }
-        ]
+            choice1: 'text-align: top;',
+            choice2: 'margin-align: top;',
+            choice3: 'overflow: top;',
+            choice4: 'vertical-align: top;',
+            answer: 4
     },
 ];
 
-var score = 0;
-var questionIndex = 0;
+//CONSTANTS
 
-var container = document.querySelector("#container");
-var currentTime = document.querySelector("#currentTime");
-var timer = document.querySelector("#startTime");
-var questionContainer = document.querySelector("#questionContainer");
+const CORRECT_BONUS = 5;
+const MAX_QUESTIONS = 12;
 
-startButton.addEventListener("click", startGame)
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    timer = 75;
+    availableQuestions = [...questions];
+    console.log(availableQuestions);
+    getNewQuestion();
+};
 
-function startGame () {
-    console.log('Started')
-    startButton.classList.add('hide')
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0;
-    questionContainerElement.classList.remove('hide')
-    setNextQuestion()
-}
+getNewQuestion = () => {
 
-function setNextQuestion() {
-    resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
-}
-
-function showQuestion(question) {
-    questionElement.innerText = question.question
-    question.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })
-}
-
-function resetState() {
-    clearStatusClass(document.body)
-    while(answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild
-        (answerButtonsElement.firstChild)
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS || timer ==== 0) {
+        // go to the end page
+        return window.location.assign("/end.html");
     }
-}
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
 
-function selectAnswer (e) {
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-}
+    choices.forEach (choice => {
+        const number = choice.dataset["number"];
+        choice.innerText = currentQuestion["choice" + number];
+    });
 
-function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add('correct')
-    } else {
-        element.classList.add('wrong')
-    }
-}
+    availableQuestions.splice(questionIndex, 1);
 
-function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
-}
+    acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+        console.log(selectedAnswer);
+
+        getNewQuestion();
+    });
+});
+
+startGame();
 
